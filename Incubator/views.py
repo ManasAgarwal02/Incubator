6f80fd6f2dcc255c805f91092d545509e75a43e6
ManasAgarwal02 <manas.99ag@gmail.com>
2020-10-07 14:37:10 +0530

    add API and storing result in json format

--- Incubator/views.py
+++ Incubator/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+import requests
+import pandas as pd
+import urllib
+from urllib import request
+
+import time
+import io
 
 
 # Create your views here.
@@ -15,5 +23,15 @@ def about(request):
 def analyze(request):
     my_url = request.POST.get("url")
     my_device = request.POST.get("device")
-    print(f"URL: {my_url}  device : {my_device}")
+    APIKey = "AIzaSyALIsdCRDWuduGIy071_x5lOFsVgyq3WaE"
+    # print(f"URL: {my_url}  device : {my_device}")
+    url = f"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={my_url}/&strategy={my_device}"
+
+    # API request
+    result = urllib.request.urlopen(url).read().decode('UTF-8')
+    # print(result)
+    result_json = json.loads(result)
+    # with open('result.json', 'w') as f:
+    #     json.dump(result_json, f)
+
     return render(request, 'incubator/analyze.html')
